9869014d5dcb51fef2f918c0b88cac00b3b68a23
dotainion <58013018+dotainion@users.noreply.github.com>
2022-05-03 01:15:01 -0400

    refactor

--- src/pages/home/OurWork.js
+++ src/pages/home/OurWork.js
@@ -9,6 +9,8 @@ export const OurWork = () =>{
     return(
         <div style={{textAlign: 'center', color: 'green'}}>
             <h1>Our Work</h1>
+            <p style={{color: 'black', width: '90%', margin: 'auto'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Why do we use it?</p>
         </div>
     )
 }
